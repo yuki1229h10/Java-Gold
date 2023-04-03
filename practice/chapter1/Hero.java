766b9c6c9fb0e5ad2ba40d75c685121fb011a63e
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2023-04-03 16:57:58 +0900

    first commit

---
+++ practice/chapter1/Hero.java
@@ -0,0 +1,10 @@
+package practice.chapter1;
+
+public class Hero {
+  private String name;
+  private int hp, mp;
+
+  public String toString() {
+    return "勇者(名前=" + this.name + "/HP=" + this.hp + "/MP=" + this.mp + ")";
+  }
+}
